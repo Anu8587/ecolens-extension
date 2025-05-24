411cda35a719bbae23c7eba3c8ff4e6ba8d300d8
Anushka Singh <anushka019bteceai23@igdtuw.ac.in>
2025-05-24 17:34:29 +0530

    setting up

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    popup: './src/popup/Popup.jsx',
+    background: './src/background.js',
+    content: './src/content_scripts/content.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
